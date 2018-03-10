4206a68001ef4e0ce8e458c7af9167af371a093f
Abel Trotter <a_trotter0@hotmail.com>
2018-03-10 11:18:40 -0800

    Show category divs of categories checked

--- assignments/assignment-6/public/scripts.js
+++ assignments/assignment-6/public/scripts.js
@@ -4,7 +4,20 @@ function startGame() {
 }
 
 function loadCategoryTitles() {
-  //set each div text to title of category selected
+  var listOfIds = checkboxList();
+  for(var i = 0; i < listOfIds.length; i++) {
+    var checkbox = listOfIds[i];
+    var categoryDiv = listOfIds[i].split('-')[0];
+    if ($(checkbox).prop('checked')) {
+      $(categoryDiv).css('display', 'inline-block');
+      console.log('Showing ' + categoryDiv + ' div');
+    }
+  }
+}
+
+function checkboxList() {
+  var list = ['#html-checkbox', '#css-checkbox', '#js-checkbox', '#jquery-checkbox', '#node-checkbox', '#mongo-checkbox'];
+  return list;
 }
 
 function displayGameBoard() {
